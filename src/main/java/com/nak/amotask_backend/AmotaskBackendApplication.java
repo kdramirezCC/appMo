9b3145909a3c6903554f9ad7aa1bf037aff852a4
kdramirezCC <kdramirezcc@fpvirtualaragon.com>
2025-07-23 16:23:09 +0200

    funciones basicas GPPPD

--- src/main/java/com/nak/amotask_backend/AmotaskBackendApplication.java
+++ src/main/java/com/nak/amotask_backend/AmotaskBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class amotaskBackendApplication {
+public class AmotaskBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(amotaskBackendApplication.class, args);
+		SpringApplication.run(AmotaskBackendApplication.class, args);
 	}
 
 }
